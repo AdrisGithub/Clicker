d5d1ae3da9550ec6ddefd6fc72b57ac9498a8201
Adrian John <adrianjohn6543@gmail.com>
2022-12-26 00:41:04 +0100

    add Timer and Score

--- src/main/java/de/example/clicker/KlickerApplication.java
+++ src/main/java/de/example/clicker/KlickerApplication.java
@@ -11,11 +11,13 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class KlickerApplication extends Application {
 
-    private static final Image circle = new Image(new File("src/main/resources/de/example/clicker/unnamed.jpg").toURI().toString());
+    private static final Image circle = new Image(new File("src/main/resources/de/example/clicker/whiteCircle.png").toURI().toString());
     private int x = 0;
     private int y = 0;
     private int score = 0;
@@ -25,24 +27,29 @@ public class KlickerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Runtime.getRuntime().exit(score);
+            }
+        },10000);
         primaryStage.setHeight(800);
         primaryStage.setWidth(800);
         primaryStage.setResizable(false);
-        primaryStage.setTitle("Drawing Operations Test");
+        primaryStage.setTitle("Click Me");
         Group root = new Group();
         Canvas canvas = new Canvas(primaryStage.getWidth(), primaryStage.getHeight());
         canvas.addEventHandler(MouseEvent.MOUSE_CLICKED,
                 t -> {
-                    System.out.println(t.getX());
-                    System.out.println(t.getY());
                     if (t.getX() >= x && t.getY() >= y && t.getX() <= x+ circle.getWidth() && t.getY() <= y+circle.getHeight() ) {
                         score++;
-                        reset(canvas, Color.WHITE,primaryStage.getWidth(), primaryStage.getHeight());
+                        reset(canvas, Color.BLACK,primaryStage.getWidth(), primaryStage.getHeight());
                     }else{
                         Runtime.getRuntime().exit(score);
                     }
                 });
-        reset(canvas, Color.WHITE,primaryStage.getWidth(), primaryStage.getHeight());
+        reset(canvas, Color.BLACK,primaryStage.getWidth(), primaryStage.getHeight());
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
@@ -52,17 +59,13 @@ public class KlickerApplication extends Application {
     private void reset(Canvas canvas, Color color,double stageX,double stageY) {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         do{
-            System.out.println("Im trying");
             x = (int) (Math.random() * canvas.getWidth());
             y = (int) (Math.random() * canvas.getHeight());
         }while(isOutOfWindow(x,y,stageX,stageY));
-        System.out.println("I made it");
-        System.out.println(x);
-        System.out.println(y);
         gc.setFill(color);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        gc.setFill(Color.BLACK);
-        gc.fillText(score+"",10,10);
+        gc.setFill(Color.WHITE);
+        gc.fillText("Score: "+score,10,15);
         gc.drawImage(circle,x,y);
     }
 
